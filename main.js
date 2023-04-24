e5e36ff205e1f7a726b5a0f7400712d26b9d450a
oliverlibaw <92615450+oliverlibaw@users.noreply.github.com>
2023-04-23 21:02:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
-(async function() {
-  if (window.MediaPipeLoadStatus) {
-    await window.MediaPipeLoadStatus;
-  }
-})();
-
-const output = document.getElementById('output');
-const eyesDetectedImage = document.getElementById('eyes-detected');
-const eyesNotDetectedImage = document.getElementById('eyes-not-detected');
+const output = document.getElementById("output");
+const eyesDetectedImage = document.getElementById("eyes-detected");
+const eyesNotDetectedImage = document.getElementById("eyes-not-detected");
 
 async function main() {
   const video = await setupCamera();
 
-  const faceMesh = new FaceMesh({locateFile: (file) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
-  }});
+  const faceMesh = new FaceMesh.FaceMesh({
+    locateFile: (file) => {
+      return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+    },
+  });
 
   faceMesh.setOptions({
     maxNumFaces: 1,
     minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5
+    minTrackingConfidence: 0.5,
   });
 
   faceMesh.onResults(onResults);
@@ -32,18 +28,19 @@ async function main() {
       eyesNotDetectedImage.hidden = false;
     }
 
-    await faceMesh.send({image: video});
+    await faceMesh.send({ image: video });
   }
 
-  await faceMesh.send({image: video});
+  await faceMesh.send({ image: video });
 }
 
 async function setupCamera() {
-  const video = document.createElement('video');
+  const video = document.createElement("video");
   video.width = 640;
   video.height = 480;
+  video.style.display = "none";
 
-  const stream = await navigator.mediaDevices.getUserMedia({video: true});
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
   video.srcObject = stream;
 
   return new Promise((resolve) => {
