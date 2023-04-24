c83d5c648bf829ae162d34cad0a8a19378c330ba
oliverlibaw <92615450+oliverlibaw@users.noreply.github.com>
2023-04-23 21:39:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,29 +3,14 @@
   const eyesDetectedImage = document.getElementById("eyes-detected");
   const eyesNotDetectedImage = document.getElementById("eyes-not-detected");
 
-  const video = document.createElement("video");
-  video.style.display = "none";
-  document.body.appendChild(video);
+  const canvas = document.createElement("canvas");
+  canvas.width = 640;
+  canvas.height = 480;
+  canvas.style.display = "none";
+  document.body.appendChild(canvas);
 
-  const MODEL_URL = "https://unpkg.com/face-api.js/models";
-
-  await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
-  await faceapi.nets.faceLandmark68TinyNet.loadFromUri(MODEL_URL);
-
-  async function detectEyes() {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks(true);
-
-    let eyesDetected = false;
-
-    for (const detection of detections) {
-      const landmarks = detection.landmarks.getLeftEye();
-      if (landmarks.length > 0) {
-        eyesDetected = true;
-        break;
-      }
-    }
-
-    if (eyesDetected) {
+  const detectEyes = () => {
+    if (JEELIZFACEFILTER.is_detected()) {
       eyesDetectedImage.hidden = false;
       eyesNotDetectedImage.hidden = true;
     } else {
@@ -34,20 +19,19 @@
     }
 
     requestAnimationFrame(detectEyes);
-  }
+  };
 
-  async function setupCamera() {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    video.srcObject = stream;
-
-    return new Promise((resolve) => {
-      video.onloadedmetadata = () => {
-        video.play();
-        resolve(video);
-      };
-    });
-  }
-
-  await setupCamera();
-  detectEyes();
+  JEELIZFACEFILTER.init({
+    canvas: canvas,
+    NNCpath: "https://unpkg.com/jeelizfacefilter/dist/",
+    callbackReady: (error) => {
+      if (error) {
+        console.log("An error occurred:", error);
+      } else {
+        console.log("JeelizFaceFilter is ready");
+        detectEyes();
+      }
+    },
+    callbackTrack: () => {},
+  });
 })();
